ced93915a0d23ad5631275724330530c1fcef583
sunnisuneel <97401783+sunnisuneel@users.noreply.github.com>
2024-02-27 15:30:12 +0530

    Update webinfra.tf

--- webinfra.tf
+++ webinfra.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "webinfra" {
     associate_public_ip_address = true
-    ami = "var.ami"
+    ami = var.ami-id
     instance_type = "t2.micro"
     key_name = "ansible"
     vpc_security_group_ids = [aws_security_group.websg.id]
